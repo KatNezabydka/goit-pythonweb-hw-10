a723c9aed1b4e4f5617e83ee1ca354b1db51875d
kateryna_kornysheva <k.kornysheva@betinvest.com>
2025-04-03 14:32:50 +0200

    add pydantic_settings

--- src/conf/config.py
+++ src/conf/config.py
@@ -2,14 +2,33 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
+#
+#
+# class Config:
+#     DB_URL = os.environ.get('DB_URL')
+#     JWT_SECRET = os.environ.get('JWT_SECRET')
+#     JWT_ALGORITHM = os.environ.get('JWT_ALGORITHM', "HS256")
+#     JWT_EXPIRATION_SECONDS = int(os.environ.get('JWT_EXPIRATION_SECONDS', 3600))
+#
+#
+# config = Config
 
 
-class Config:
-    DB_URL = os.environ.get('DB_URL')
-    JWT_SECRET = os.environ.get('JWT_SECRET')
-    JWT_ALGORITHM = os.environ.get('JWT_ALGORITHM', "HS256")
-    JWT_EXPIRATION_SECONDS = int(os.environ.get('JWT_EXPIRATION_SECONDS', 3600))
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
+class Settings(BaseSettings):
+    DB_URL: str
+    JWT_SECRET: str
+    JWT_ALGORITHM: str = "HS256"
+    JWT_EXPIRATION_SECONDS: int = Field(default=3600)
 
-config = Config
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+        case_sensitive=True
+    )
+
+settings = Settings()

7fc17f51b8bb8da31727b8c459d2d2e999e1b995
kateryna_kornysheva <k.kornysheva@betinvest.com>
2025-04-03 10:53:17 +0200

    add env

--- src/conf/config.py
+++ src/conf/config.py
@@ -1,7 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class Config:
-    DB_URL = "postgresql+asyncpg://postgres:567234@postgres:5432/contacts"
-    JWT_SECRET = "xjiQi5T-_DH9sfYEJnC1Df74rHrM30Oi4PS4sM6PFNM"  # Секретний ключ для токенів
-    JWT_ALGORITHM = "HS256"  # Алгоритм шифрування токенів
-    JWT_EXPIRATION_SECONDS = 3600  # Час дії токена (1 година)
+    DB_URL = os.environ.get('DB_URL')
+    JWT_SECRET = os.environ.get('JWT_SECRET')
+    JWT_ALGORITHM = os.environ.get('JWT_ALGORITHM', "HS256")
+    JWT_EXPIRATION_SECONDS = int(os.environ.get('JWT_EXPIRATION_SECONDS', 3600))
+
 
 config = Config
